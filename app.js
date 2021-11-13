37916b17e5c8428cdbe982f32d5c6a6d44b9c07a
devyosse <narutoshippuden884@gmail.com>
2021-11-13 10:39:01 +0100

    exo 53

---
+++ app.js
@@ -0,0 +1,24 @@
+let texte = document.getElementById('texte');
+texte.style.color = 'blue';
+texte.style.fontStyle = '13px'
+
+let texte2 = document.getElementById('texte2');
+texte2.style.color = 'blue';
+texte2.style.fontStyle = '13px'
+
+let texte3 = document.getElementById('texte3');
+texte3.style.color = 'blue';
+texte3.style.fontStyle = '13px'
+
+let texte4 = document.getElementById('texte4')
+texte4.style.color = 'orange';
+texte4.style.fontStyle = '13px';
+texte4.style.fontWeight = 'italic'
+
+let anime = document .getElementsByClassName('anime');
+anime.style.animation = 'animation-name';
+anime.style.animationDuration = '10s';
+
+let element  = document.getElementsByClassName('spanTest');
+element.style.backgroundColor = 'red';
+element.dir = 'rtl';
